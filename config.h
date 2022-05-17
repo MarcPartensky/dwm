d0ae5da80cd7a6e4fdfd5503c302c87143a8bf9e
Marc Partensky <marc.partensky@gmail.com>
2022-05-18 00:35:18 +0200

    rotate stack that works

--- config.h
+++ config.h
@@ -97,6 +97,8 @@ static Key keys[] = {
 	{ Mod1Mask,                     XK_s,      spawn,          {.v = spotifyprevious } },
 	{ Mod1Mask,                     XK_k,      spawn,          {.v = spotifytoggle } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
+ 	{ MODKEY|ShiftMask,             XK_j,      rotatestack,    {.i = +1 } },
+ 	{ MODKEY|ShiftMask,             XK_k,      rotatestack,    {.i = -1 } },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
