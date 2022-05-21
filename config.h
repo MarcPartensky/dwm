3404bc1685f898919a268dd21131b5ce1a160a10
Marc Partensky <marc.partensky@gmail.com>
2022-05-22 00:36:32 +0200

    teams

--- config.h
+++ config.h
@@ -22,20 +22,21 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" };
 
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class        instance    title         tags mask     isfloating   monitor */
-	{ "Gimp",       NULL,       NULL,         0,            1,           -1 },
-	{ "Firefox",    NULL,       NULL,         1 << 1,       0,           -1 },
-	{ "Spotify",    NULL,       "Spotify",    1 << 2,       0,           -1 },
-	{ NULL,         NULL,       "Discord",    1 << 3,       0,           -1 },
-	{ NULL,         NULL,       "Messenger",  1 << 4,       0,           -1 },
-	{ NULL,         NULL,       "Slack",      1 << 8,       0,           -1 },
+	/* class        instance    title              tags mask  isfloating  monitor */
+	{ "Gimp",       NULL,       NULL,              0,         1,          -1 },
+	{ "Firefox",    NULL,       NULL,              1 << 1,    0,          -1 },
+	{ "Spotify",    NULL,       "Spotify",         1 << 2,    0,          -1 },
+	{ NULL,         NULL,       "Discord",         1 << 3,    0,          -1 },
+	{ NULL,         NULL,       "Messenger",       1 << 4,    0,          -1 },
+	{ NULL,         NULL,       "Microsoft Teams", 1 << 7,    0,          -1 },
+	{ NULL,         NULL,       "Slack",           1 << 8,    0,          -1 },
 };
 
 /* layout(s) */
